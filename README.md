6e5794c479673f2915f486aafa7928855291cb67
bayuTiger <bayutoranosuke@gmail.com>
2022-12-26 05:17:15 +0900

    add:home,script

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 
 ## 環境
 
-- laravel: 8.6.12
-- vue: 2.6.12
+- Laravel: 8.6.2
+- Vue.js: 2.6.12
+- PHP: 8.0.8
+- Composer: 2.5.1
+- Node.js: 19.3.0
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,28 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Vueお試し</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <input v-model="name">
+                    <p>名前： @{{ name }}</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('js')
+<script>
+    const app = new Vue({
+        el: '#app',
+        data: () => {
+            return {
+                name: '',
+            }
+        },
+        methods: {}
+    });
+</script>
 @endsection
